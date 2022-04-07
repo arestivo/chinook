076c9d5a92994a6eaf3e7a52a4e7a2e26b2de7d1
André Restivo <arestivo@carbon.local>
2022-04-07 15:02:02 +0100

    Fix h3 closing tag

--- templates/album.tpl.php
+++ templates/album.tpl.php
@@ -2,7 +2,7 @@
 
 <?php function drawAlbum(int $id, string $albumName, int $artistId, string $artistName, array $tracks) { ?>
   <h2><?=$albumName?></h2>
-  <h3><a href="artist.php?id=<?=$artistId?>"><?=$artistName?></a></h2>      
+  <h3><a href="artist.php?id=<?=$artistId?>"><?=$artistName?></a></h3>      
   <table id="tracks">
     <tr><th scope="col">#</th><th scope="col">Title</th><th scope="col">Duration</th></tr>
     <?php foreach ($tracks as $track) { ?>
